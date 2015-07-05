f9f63b76f2cabe4d0a4d56fb13cd2539129ef6d3
shymega <shymega@users.noreply.github.com>
2015-07-05 15:10:28 +0100

    [Mod]: Modified script for test coverage

--- script/test-coverage.sh
+++ script/test-coverage.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
-echo "mode: set" > acc.out
-for Dir in $(find ./* -maxdepth 2 -type d ); 
-do
-	if ls $Dir/*.go &> /dev/null;
-	then
-		returnval=`go test -coverprofile=profile.out $Dir`
-		echo ${returnval}
-		if [[ ${returnval} != *FAIL* ]]
-		then
-    		if [ -f profile.out ]
-    		then
-        		cat profile.out | grep -v "mode: set" >> acc.out 
-    		fi
-    	else
-    		exit 1
-    	fi	
-    fi
-done
-if [ -n "$COVERALLS" ]
-then
-	goveralls -coverprofile=acc.out $COVERALLS
-fi	
+# run on CI service w/something like:
+#
+# go get github.com/axw/gocov/gocov
+# go get github.com/mattn/goveralls
+# COVERALLS="-service drone.io -repotoken $COVERALLS_TOKEN" ./test-coverage.sh
+#
 
-rm -rf ./profile.out
-rm -rf ./acc.out
+echo "mode: set" > acc.out
+fail=0
+
+# Standard go tooling behavior is to ignore dirs with leading underscors
+for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_*' -type d);
+do
+  if ls $dir/*.go &> /dev/null; then
+    go test -coverprofile=profile.out $dir || fail=1
+    if [ -f profile.out ]
+    then
+      cat profile.out | grep -v "mode: set" >> acc.out
+      rm profile.out
+    fi
+  fi
+done
+
+# Failures have incomplete results, so don't send
+if [ -n "$COVERALLS" ] && [ "$fail" -eq 0 ]
+then
+  goveralls -v -coverprofile=acc.out $COVERALLS
+fi
+
+rm -f acc.out
+
+exit $fail
